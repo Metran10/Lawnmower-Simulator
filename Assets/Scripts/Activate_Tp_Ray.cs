b7c2cbc05c9161ecaa060ec5aedea86fd869c3d8
Metran10 <grzetra@op.pl>
2024-01-04 19:49:49 +0100

    Menu UI, changing levels, turn type, showing lvl menu adn improvements to main menu scene

--- Assets/Scripts/Activate_Tp_Ray.cs
+++ Assets/Scripts/Activate_Tp_Ray.cs
@@ -15,11 +15,16 @@ public class Activate_Tp_Ray : MonoBehaviour
     public InputActionProperty leftCancel;
     public InputActionProperty rightCancel;
 
+    public XRRayInteractor leftRay;
+    public XRRayInteractor rightRay;
 
     // Update is called once per frame
     void Update()
     {
-        leftTP.SetActive(leftCancel.action.ReadValue<float>() == 0 &&  leftActivation.action.ReadValue<float>() > 0.1f);
-        rightTP.SetActive(rightCancel.action.ReadValue<float>() == 0 && rightActivation.action.ReadValue<float>() > 0.1f);
+        bool isLeftRayHovering = leftRay.TryGetHitInfo(out Vector3 leftPose, out Vector3 leftNormal, out int leftPosLine, out bool leftIsValidTarget);
+        bool isRightRayHovering = rightRay.TryGetHitInfo(out Vector3 rightPose, out Vector3 rightNormal, out int rightPosLine, out bool rightIsValidTarget);
+
+        leftTP.SetActive(!isLeftRayHovering && leftCancel.action.ReadValue<float>() == 0 &&  leftActivation.action.ReadValue<float>() > 0.1f);
+        rightTP.SetActive(!rightIsValidTarget && rightCancel.action.ReadValue<float>() == 0 && rightActivation.action.ReadValue<float>() > 0.1f);
     }
 }
---
+++ Assets/Scripts/GameMenuManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class GameMenuManager : MonoBehaviour
+{
+    public Transform playerHead;
+    public float spawnDistance = 2;
+    public GameObject menu;
+    public InputActionProperty showButton;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (showButton.action.WasPressedThisFrame())
+        {
+            menu.SetActive(!menu.activeSelf);
+
+            menu.transform.position = playerHead.position + new Vector3(playerHead.forward.x, 0, playerHead.forward.z).normalized * spawnDistance;
+        }
+
+        menu.transform.LookAt(new Vector3(playerHead.position.x, menu.transform.position.y, playerHead.position.z));
+        menu.transform.forward *= -1;
+
+    }
+
+    public void GoToMenu()
+    {
+        SceneManager.LoadScene("Main_Menu");
+    }
+
+
+}
