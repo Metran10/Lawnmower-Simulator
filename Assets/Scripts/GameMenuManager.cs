10710a78a186cb8aa041e6f62809bc8e636770da
Metran10 <grzetra@op.pl>
2024-01-09 15:07:24 +0100

    Basic LVL1 setup and fixes

--- Assets/Scripts/GameMenuManager.cs
+++ Assets/Scripts/GameMenuManager.cs
@@ -15,7 +15,7 @@ public class GameMenuManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        menu.SetActive(false);
     }
 
     // Update is called once per frame
